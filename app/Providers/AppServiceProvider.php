70f66b647bf88baa38f1c81efa236df2ec25d5ae
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-25 14:44:24 +0100

    Authorization w/ Gates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Enums\Role;
+use App\Models\Image;
+use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +25,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        Gate::define('update-image', function (User $user, Image $image) {
+            return $user->id === $image->user_id || $user->role === Role::Editor;
+        });
+
+        Gate::define('delete-image', function (User $user, Image $image) {
+            return $user->id === $image->user_id;
+        });
     }
 }
