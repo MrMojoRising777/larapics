ef82f44c6d4b3b8ca7d68cbc0514af0949d6a75d
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-27 15:49:13 +0100

    Modify user profile schema

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,10 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Provider\en_US\Address;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -23,9 +24,15 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $this->faker->addProvider(new Address($this->faker));
+        
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => $email = fake()->unique()->safeEmail(),
+            'username' => strstr($email, '@', true) . rand(100, 120),
+            'city' => rand(0, 1) === 0 ? null : $this->faker->city(),
+            'country' => rand(0, 1) === 0 ? null : $this->faker->country(),
+            'about_me' => rand(0, 1) === 0 ? null : $this->faker->text(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
